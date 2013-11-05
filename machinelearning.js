5de55f920c7d0ecf60d0b686f772c26713884c22
Margarita <scarlett_11.04@mail.ru>
2013-11-05 09:55:04 +0700

    fgfgf

--- machinelearning.js
+++ machinelearning.js
@@ -1,168 +1,29 @@
-/*********************************************************
-    ALGEBRA
-**********************************************************/
-
-function matrixMultiply(a,b){
-        var btrans = transposeMatrix(b);
-        var result = [];
-        for(var i=0; i < a.length; i++){
-                var row = [];
-                for(var j=0; j < btrans.length; j++){
-                        var value = internalProduct(a[i],btrans[j]);
-                        row.push(value)
-                }
-                result.push(row);
-        }
-        return result;
-}
-
-function matrixScalarMultiply(m,s){
-        var result = [];
-        for(var i=0; i < m.length; i++){
-                var row = [];
-                for(var j=0; j < m[0].length; j++){
-                        row.push(s * m[i][j]);
-                }
-                result.push(row);
-        }
-        return result;
-}
-
-
-//unnecessary - use dotOp instead
-function matrixAdd(a,b){
-        var result = [];
-        for(var i=0; i < a.length; i++){
-                var row = [];
-                for(var j=0; j < a[0].length; j++){
-                        row.push(a[i][j] + b[i][j]);
-                }
-                result.push(row);
-        }
-        return result;
-}
-
-//for internal usage only
-function internalProduct(u,v){
-        if (u.length != v.length) throw "SizesDoNotMatch";
-        var sum = 0;
-        for(var i=0; i < u.length; i++){
-                sum += u[i]*v[i];
-        }
-        return sum;
-}
-
-function transposeMatrix(m){
-        var t = [];
-        for(var i=0; i < m[0].length; i++){
-                var row = [];
-                for(var j=0; j < m.length; j++){
-                        row.push(m[j][i]);
-                }
-                t.push(row);
-        }
-        return t;
-}
-
-function minorMatrix(m, k, l){
-        var reduced = [];
-        for(var i=0; i < m.length; i++){
-                if(i==k) continue;
-                var row = [];
-                for(var j=0; j < m.length; j++){
-                        if(j==l) continue;
-                        row.push(m[i][j])
-                }
-                reduced.push(row);
-        }
-        return reduced;
-}
-
-function determinant(m){
-        var size = m.length;
-        if(size == 1) return m[0][0];
-        if(size == 2) return m[0][0] * m[1][1] - m[0][1] * m[1][0];
-        var det = 0;
-        for(var i=0; i < size; i++){
-                var minor = minorMatrix(m,0,i);
-                var signal = (i%2 > 0) ? -1 : 1;
-                det += signal * m[0][i]* determinant(minor);
-        }
-        return det;
-}
-
-
-function cofactor(m, k, l){
-        minor = minorMatrix(m, k, l);
-        return determinant(minor);
-}
-
-
-function cofactorMatrix(m){
-        var cofactors = [];
-        for(var i = 0; i < m.length; i++){
-                var row = [];
-                for(var j = 0; j < m.length; j++){
-                        var cofactorval = cofactor(m,i,j);
-                        row.push(cofactorval);
-                }
-                cofactors.push(row);
-        }
-        return cofactors;
-}
-
-function inverseMatrix(m){
-        var det = determinant(m);
-        if (det == 0) throw "SingularMatrix";
-        var deti = 1 / det;
-        var cof = cofactorMatrix(m);
-        var adj = transposeMatrix(cof);
-        var result = matrixScalarMultiply(adj,deti);
-        return result;
-}
-
-
-function dotOp(func,m,n){
-        var result = [];
-        for(var i = 0; i < m.length; i++){
-                var row = [];
-                for(var j = 0; j < m[0].length; j++){
-                        row.push(func(m[i][j], n[i][j]));
-                }
-                result.push(row);
-        }
-        return result;
-}
-
-
-function generateMatrix(nlines, ncols, func){
-        var m = [];
-        for(var i = 0; i < nlines; i++){
-                var row = [];
-                for(var j = 0; j < ncols; j++){
-                        row.push(func(i,j));
-                }
-                m.push(row);
-        }
-        return m;
-}
-
-function zeros(nlines,ncols){
-        return generateMatrix(nlines,ncols, function(i,j){return 0;});
-}
-
-function ones(nlines,ncols){
-        return generateMatrix(nlines,ncols, function(i,j){return 1;});
-}
-
-function identity(size){
-        return generateMatrix(size, size, function(i,j){ if(i===j) return 1; return 0; });
-}
-
-/**********************************************************************************************/
-
-
 $(document).ready( function() {
+    
+    var board = JXG.JSXGraph.initBoard('box', {boundingbox:[-1,3,2,-3], axis:true});
+    var f, curve; // global objects
+    
+    function plotter(y_init) {
+  var txtraw = y_init;
+  f = board.jc.snippet(txtraw, true, 'x', true);
+  curve = board.create('functiongraph',[f,
+                function(){ 
+                  var c = new JXG.Coords(JXG.COORDS_BY_SCREEN,[0,0],board);
+                  return c.usrCoords[1];
+                },
+                function(){ 
+                  var c = new JXG.Coords(JXG.COORDS_BY_SCREEN,[board.canvasWidth,0],board);
+                  return c.usrCoords[1];
+                }
+              ],{name:txtraw, withLabel:true});
+  var q = board.create('glider', [2, 1, curve], {withLabel:false});
+  var t = board.create('text', [
+          function(){ return q.X()+0.1; },
+          function(){ return q.Y()+0.1; },
+          function(){ return "The slope of the function f(x)=" + txtraw + "<br>at x=" + q.X().toFixed(2) + " is equal to " + (JXG.Math.Numerics.D(f))(q.X()).toFixed(2); }
+      ], 
+      {fontSize:15});
+};
     
     var y_init1 = function(x) {
         return Math.cos(2*Math.PI*x);
@@ -200,10 +61,16 @@ $(document).ready( function() {
             for (var j=0; j<=M; j++)
                 A[i][j] = Math.pow(x[i], j);
         console.log(A);
-        if (y_init=='func-init1') {
-            var y = new Array (n);    
-            for (var i=0; i<n; i++) 
-                y[i] = y_init1( x[i] );
+        if (y_init=='cos(2*PI*x)') {
+            var y = new Array (M+1);    
+            for (var i=0; i<=M; i++) 
+                y[i] = Array(n);
+            
+            for (var i=0; i<=M; i++) 
+                y[i][0] = y_init1( x[i] );
+            for (var i=1; i<=M; i++)
+                for (var j=0; j<n; j++)
+                    y[i][j] = 0;
         }
         
         if (y_init=='func-init2') {
@@ -217,38 +84,21 @@ $(document).ready( function() {
             for (var i=0; i<n; i++) 
                 y[i] = y_init3( x[i] );
         }
+       
+        var At = transposeMatrix(A);
+        var AA = matrixMultiply(inverseMatrix(matrixMultiply(At,A)),At)
+        var w = matrixMultiply(AA,y);
         
-       var w = Gauss(A, y, M, n); 
+       //var w = Gauss(A, y, M, n); 
             
        var res = '';
        for (var i=0; i<=M; i++)
           res += w[i][0]+'*x^'+i+'+';
        $('.res').text('Polinom f(x,w) = '+res.slice(0, -1));
         
+        plotter(y_init);
+        plotter(res.slice(0, -1));
         
-    /*    switch (y_init) {
-            case 'func-init1': 
-                var y = new Array(); console.log('4!!!!!!!!!!!!');
-                for (var i=0; i<=M; i++) 
-                    y[i] = y_init1( x[i] );
-                
-                var Ai = inverseMatrix(A); console.log('5!!!!!!!!!!!!');
-                var w = matrixMultiply(Ai,y); console.log(w);
-                break;
-                
-            case 'func-init2':
-                break;
-                
-            case 'func-init2':
-                break;   
-        }
-        */
-        
-        /*var data = new Array(n);
-        for (var i=0; i<n; i++)
-            data[i] = [x[i], polinom(x[i], w, M)];
-        
-        var plot = $.plot($('.graph'), data)*/
         
     });
    
@@ -311,7 +161,7 @@ var Gauss = function(a, b, M, n) {
         //делим i-е уравнение на a[i][j]
         for (var l = j+1; l < M+1; ++l) {
             a[i][l] /= a[i][j];
-        }
+        } console.log(a[i][j]); console.log(b[i]);
         b[i] /= a[i][j];
         a[i][j] = 1;
         //путём элементарных преобразований обнулить
@@ -374,6 +224,11 @@ var Gauss = function(a, b, M, n) {
     }
     return ans;
 };    
+
+
+
+ 
+
     
     
     
